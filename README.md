1fc7ab27e2fea0e8a68674e6af648f2bf7dc8871
Luke Turner <luke.ian.turner@outlook.de>
2020-11-23 16:06:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JiraEnginePlugin
+
+Developed with Unreal Engine 4
---
+++ Source/JiraEnginePlugin/JiraEnginePlugin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JiraEnginePlugin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JiraEnginePlugin, "JiraEnginePlugin" );
